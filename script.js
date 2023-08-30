89d560384ea8eb16268b1143662a042cb5e2f8f9
Joakim Villo <villokodehode@gmail.com>
2023-08-30 09:58:53 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,111 @@
+
+// console.log skriver ut til consollen i developer tools på nettsiden
+console.log("hello world!")
+
+
+// let definerer en variabel som får lov til å endres på senere
+let ord1 = "hello"
+
+const ord2 = "World!"
+
+console.log(ord1 + " " + ord2)
+
+// hvordan å finne lengden av objekter (i dette tilfelle bokstaver):
+console.log((ord1 + ord2).length)
+
+let setning = ord1 + " " + ord2
+
+console.log(setning)
+
+
+let nummer = 1
+const nummer2 = 2
+// sum skal være = 3
+let sum = nummer + nummer2 
+
+console.log("nummer er: " + nummer)
+console.log("summen er: " + sum + " (før nummer øker med 1)")
+
+// øker nummer med 1:
+nummer++
+
+console.log("resultatet skal være 2: " + nummer)
+
+
+// console.log(sum)
+
+// Hvordan å få et tilfeldig tall mellom 1 og 10:
+const randomNumber = Math.random() * 9 + 1
+console.log(randomNumber)
+const roundedNumber = Math.round(randomNumber)
+console.log(roundedNumber)
+
+
+// datatyper:
+// String: "Hello world!"
+// Integer/number: Alle heltall (1, 2, 3, 548237)
+// Float: Alle tall med desimaler (1,65)
+// Boolean: True eller false
+// Array: f.eks let fruit = ["kiwi", "apple", "pear"]
+// Object: 
+// Undefined: const person = {firstName: "Jokka", lastName: "Bolle", age: "65" }
+
+
+let fruitsArray = ["banan", "jordbær", "tomat", "kiwi"]
+
+console.log(fruitsArray)
+
+console.log(fruitsArray[1])
+
+fruitsArray.push("eple")
+
+console.log(fruitsArray)
+fruitsArray.pop()
+console.log(fruitsArray)
+console.log(fruitsArray.length)
+
+// nytt i JS. Man behøver ikke definere variabler, men allikevel GJØR DET
+ord3 = "halla på deg!"
+
+console.log(ord3)
+
+const persons = [{
+
+    firstName: "Joakim",
+    lastName: "Villo",
+    age: 35,
+    hobbies: ["Gaming", "Trening", "Filmer"]
+},
+{
+firstName: "Bob",
+lastName: "Dylan",
+age: 80,
+hobbies: ["Music", "MJ", "Jamaican food"]}
+]
+
+const Joakim = persons[0]
+const Bob = persons[1]
+
+const BobAge = Bob.age
+
+console.log(Joakim.firstName)
+console.log(Bob.firstName)
+console.log(BobAge)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
